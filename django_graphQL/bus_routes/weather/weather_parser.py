1c439dfb0247be21db69e8cd7c0240434020c3e0
Jonathan Farrell <jonathan.farrell11@gmail.com>
2021-08-03 11:24:13 +0100

    weather parser updated to include uvi

--- django_graphQL/bus_routes/weather/weather_parser.py
+++ django_graphQL/bus_routes/weather/weather_parser.py
@@ -1,5 +1,5 @@
 import requests
-from .apikey import api
+from .config import api
 
 
 def weather_info():
@@ -7,7 +7,9 @@ def weather_info():
                        "feels_like": 0,
                        "pressure": 0,
                        "wind_speed": 0,
-                       "weather": 0}
+                       "uvi": 0,
+                       "weather": 0,
+                       "icon": ""}
 
     forecast = requests.get(api)
     response = forecast.status_code
@@ -18,10 +20,12 @@ def weather_info():
     i = forecast_data["current"]
     current_weather["temp"] = i["temp"]
     current_weather["feels_like"] = i["feels_like"]
+    current_weather["uvi"] = i["uvi"]
     current_weather["pressure"] = i["pressure"]
     current_weather["wind_speed"] = i["wind_speed"]
 
     j = (i["weather"])
     j = (j[0])
     current_weather["weather"] = j["main"]
+    current_weather["icon"] = j["icon"]
     return current_weather
