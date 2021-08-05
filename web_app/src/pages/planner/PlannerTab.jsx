6b86f27b439b19d0263b580b3d6862d4cc143016
Tom Walsh <tomwalsh96@gmail.com>
2021-08-05 18:51:39 +0100

    error in type

--- web_app/src/pages/planner/PlannerTab.jsx
+++ web_app/src/pages/planner/PlannerTab.jsx
@@ -6,7 +6,7 @@ function PlannerTab() {
     <div>
 
       <h1>Plan your route</h1>
-      <p>Enter an origin and destination and we will find the best route using the dublin bikes network</p>
+      <p>Enter an origin and destination and we will find the best route using public transport</p>
       <br/>
 
       <GoogleDirections />
