ced5c9a93ce256cad92054671058b2445cdeb994
Tom Walsh <tomwalsh96@gmail.com>
2021-08-10 19:03:00 +0100

    finished route arrival times at stop query

--- django_graphQL/bus_routes/schema.py
+++ django_graphQL/bus_routes/schema.py
@@ -2,8 +2,6 @@ import graphene
 from .types import *
 from .weather import weather_parser
 import pickle
-from .live_data import live_api as live
-from .live_data import to_timestamp
 
 
 class Query(graphene.ObjectType):
@@ -13,7 +11,9 @@ class Query(graphene.ObjectType):
                                  hour=graphene.String(required=True),
                                  month=graphene.String(required=True),
                                  rain=graphene.String(required=True),
-                                 temp=graphene.String(required=True))
+                                 temp=graphene.String(required=True),
+                                 list_size=graphene.Int(required=True),
+                                 stop_num=graphene.String(required=True))
 
     stops_on_route = graphene.List(UniqueRoutesType,
                                    route_num=graphene.String(required=True),
@@ -36,40 +36,71 @@ class Query(graphene.ObjectType):
         weather_dict = str(weather_dict)
         return weather_dict
 
-    def resolve_stops_on_route(root, info, route_num, direction):
+    def resolve_stops_on_route(root, info, line_id, direction):
         for route in UniqueRoutes.objects.all():
-            if route.route_num == route_num and route.direction == direction:
-                return route
+          if route.line_id == line_id and route.direction == direction:
+              return route
 
-    def resolve_prediction(root, info, route, direction, day, hour, month, rain, temp):
-        # grab overall prediction time for route journey
-        test = pickle.load(open(f'./bus_routes/route_models/inbound/RandForest_{route}.pkl', 'rb'))
-        predicted_journey_time = test.predict([[day, hour, month, rain, temp]])
+    def resolve_prediction(root, info, route, direction, day, hour, month, rain, temp, list_size, stop_num):
+        
+        # get relevant models pickle file
+        model = pickle.load(open(f'./bus_routes/route_models/{direction}/RandForest_{route}.pkl', 'rb'))
 
-        # get the details of the requested route
-        live_departures = live.live_data(route, direction)
-        print(live_departures)
+        # get all departure times for route
+        allDepartureTimes = []
+        for i in UniqueRoutes.objects.all():
+          if (i.line_id == route and i.direction == direction):
+            allDepartureTimes = [x.strip() for x in i.first_departure_schedule.split(',')]
+
+        # get all travel times
+        allTravelTimes = []
+        for i in allDepartureTimes:
+          hr = i.split(":")[0].strip("0")
+          allTravelTimes.append(model.predict([[day, hr, month, rain, temp]])[0])
+
+        # for redundancy I will include atleast 10 times past the current day to allow wrap around
+        extras = 0
+        # ----------------------------------------------------------------------------------------------------------
+        # come back to this
+        # ----------------------------------------------------------------------------------------------------------
+
+        # predict each arrival time at chosen stop
         route_data = Query.resolve_stops_on_route(root, info, route, direction)
-        all_stops = route_data.stops.split(",")
-        num_stops = len(all_stops)
+        allStops = [x.strip() for x in route_data.stops.split(",")]
+        position = 0
+        for i in allStops:
+          if stop_num == i:
+            break
+          else:
+            position += 1
+        numStops = len(allStops)
+        allArrivalTimes = []
+        x = 0
+        for i in allTravelTimes:
+          timeDep =  allDepartureTimes[x].split(':')
+          departureTimeInSeconds = int(timeDep[0])*60*60 + int(timeDep[1])*60 + int(timeDep[2])
+          travelTimeToStopInSeconds = (i/numStops)*position
+          arrivalTimeInSeconds = (departureTimeInSeconds + travelTimeToStopInSeconds)
 
-        times = []
-        for bus in live_departures:
-            time = bus[0]
-            ftr = [3600, 60, 1]
-            dep_time_seconds = sum([a*b for a, b in zip(ftr, map(int, time.split(':')))])
-            time_per_stop = predicted_journey_time/num_stops
-            print(time_per_stop)
+          arrivalSecond = int(arrivalTimeInSeconds % 60)
+          remainder = arrivalTimeInSeconds // 60
+          arrivalMinute = int(remainder % 60)
+          arrivalHour = int(remainder // 60)
 
-            temp = []
-            for stop in range(len(all_stops)):
-                predicted_arrival = dep_time_seconds + (time_per_stop * stop)
-                predicted_time = to_timestamp.to_time(predicted_arrival)
+          arrivalTime = str(arrivalHour) + ":" + str(arrivalMinute) + ":" + str(arrivalSecond)
+          x += 1
+          if (x > len(allDepartureTimes)):
+            x=0
+          allArrivalTimes.append(arrivalTime)
 
-                temp.append([all_stops[stop], predicted_time])
-            times.append(temp)
+        # get next x arriving buses, x being provided as "list_size"
+        nextArrivalTimes = []
+        for i in allArrivalTimes:
+          if (int(hour) <= int(i.split(':')[0]) and len(nextArrivalTimes) <= list_size):
+            nextArrivalTimes.append(i)
 
-        return str(times)
+        # return data
+        return str(nextArrivalTimes)
 
 
 schema = graphene.Schema(
